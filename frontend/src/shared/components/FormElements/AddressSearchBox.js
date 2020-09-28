5b33ba5b1d38e2cc259fc75681266e39fe31eab9
mosaadaldeen <mohammadsd97@gmail.com>
2020-09-28 16:37:38 +0200

    merged social media feature

--- frontend/src/shared/components/FormElements/AddressSearchBox.js
+++ frontend/src/shared/components/FormElements/AddressSearchBox.js
@@ -1,26 +1,25 @@
 import React, { useRef, useEffect } from "react";
 import mapboxgl from "mapbox-gl";
-import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 
-import './AddressSearchBox.css'
+import "./AddressSearchBox.css";
 
 function AddressSearchBox(props) {
-    const boxRef = useRef();
-    useEffect(() => {
-        mapboxgl.accessToken =
-        "pk.eyJ1Ijoib21lcmJrazA2IiwiYSI6ImNrNThqeGRxbDBlcDIzam11eDIzaDRsamYifQ.P8GSW1O3dRj59x7v9l7FqA";
-      var geocoder = new MapboxGeocoder({
-        accessToken: mapboxgl.accessToken,
-        type:'country'
-      });
-    
-      geocoder.addTo(boxRef.current);
-    
-    },[])
-      
+  const boxRef = useRef();
+  useEffect(() => {
+    mapboxgl.accessToken =
+      "pk.eyJ1Ijoib21lcmJrazA2IiwiYSI6ImNrNThqeGRxbDBlcDIzam11eDIzaDRsamYifQ.P8GSW1O3dRj59x7v9l7FqA";
+    var geocoder = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      type: "country",
+    });
+
+    geocoder.addTo(boxRef.current);
+  }, []);
+
   return (
     <>
-      <label className='address-label'>Address</label>
+      <label className="address-label">Address</label>
       <div id={props.id} ref={boxRef}></div>
     </>
   );
