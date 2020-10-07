966af738a7ee9a949407251e44e27f79b9b9ee2f
zekiye <zekiyecakiral@gmail.com>
2020-10-07 20:57:39 +0200

    follow works

--- frontend/src/places/pages/UserPlaces.js
+++ frontend/src/places/pages/UserPlaces.js
@@ -32,14 +32,6 @@ const UserPlaces = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [placesPerPage] = useState(3);
 
-  const [followers, setFollowers] = useState();
-  const [following, setFollowing] = useState();
-  const [loginUserFollowing, setLoginUserFollowing] = useState();
-  const [followStatus, setFollowStatus] = useState(false);
-
-  const [showFollowers, setShowFollowers] = useState(false);
-  const [showFollowing, setShowFollowing] = useState(false);
-
   const userId = useParams().userId;
 
   useEffect(() => {
@@ -48,32 +40,12 @@ const UserPlaces = () => {
         const responseData = await sendRequest(
           `${process.env.REACT_APP_BACKEND_URL}/places/user/${userId}`
         );
+        console.log('deneme',responseData);
         setUserDetail({ image: responseData.userWithPlaces.image });
         setLoadedPlaces(responseData.places);
         setUserInfo(responseData.userWithPlaces);
       } catch (err) {}
     };
-
-    // const fetchFollowList = async () => {
-    //   try {
-    //     const responseData = await sendRequest(
-    //       `${process.env.REACT_APP_BACKEND_URL}/follow/list/${userId}`
-    //     );
-    //     if (
-    //       responseData.follow[0].followers.filter((x) => x._id === auth.userId)
-    //     ) {
-    //       setFollowStatus(true);
-    //     }
-    //     setFollowing(responseData.follow[0].following);
-    //     setFollowers(responseData.follow[0].followers);
-
-    //     const loginUserData = await sendRequest(
-    //       `${process.env.REACT_APP_BACKEND_URL}/follow/list/${auth.userId}`
-    //     );
-    //     console.log('loginuser', loginUserData.follow[0].following);
-    //     setLoginUserFollowing(loginUserData.follow[0].following);
-    //   } catch (err) {}
-    // };
 
     fetchPlaces();
     // fetchFollowList();
@@ -94,51 +66,8 @@ const UserPlaces = () => {
   // Change the places when the pagination pageNumber clicked
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
-  // const followHandler = async (event) => {
-  //   event.preventDefault();
-  //   console.log('follow');
-  //   try {
-  //     const responseData = await sendRequest(
-  //       `${process.env.REACT_APP_BACKEND_URL}/follow/${auth.userId}`,
-  //       'POST',
-  //       JSON.stringify({
-  //         follow_id: userId,
-  //       }),
-  //       {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       500
-  //     );
-  //     setFollowStatus(true);
-  //     setFollowers(responseData.followers);
-  //     //setFollowingCount(responseData.followingCount);
-  //     console.log(responseData);
-  //   } catch (err) {}
-  // };
 
-  // const unfollowHandler = async (event) => {
-  //   event.preventDefault();
-  //   try {
-  //     const responseData = await sendRequest(
-  //       `${process.env.REACT_APP_BACKEND_URL}/follow/unfollow/${auth.userId}`,
-  //       'POST',
-  //       JSON.stringify({
-  //         follow_id: userId,
-  //       }),
-  //       {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       500
-  //     );
-  //     console.log('unfollow', responseData.followers);
-  //     setFollowers(responseData.followers);
-  //     setFollowStatus(false);
-  //   } catch (err) {}
-  // };
 
-  const closeFollowers = () => {
-    setShowFollowers(false);
-  };
 
 
 
@@ -146,42 +75,6 @@ const UserPlaces = () => {
     <React.Fragment>
       <ErrorModal error={error} onClear={clearError} />
 
-      {/* <Modal
-        className='modal-follow-content'
-        show={showFollowers}
-        onCancel={closeFollowers}
-      >
-        {followers &&
-          followers.map((item) => {
-            return (
-              <div className='modal-follow'>
-                <div className='modal-info'>
-                  <Avatar
-                    alt='profile'
-                    src={`${process.env.REACT_APP_ASSET_URL}/${item.image}`}
-                    // aria-controls={menuId}
-                    // onClick={handleProfileMenuOpen}
-                    className={classes.large}
-                  />
-                  <Typography className='follow-name'>{item.name}</Typography>
-                </div>
-                <div>
-                  {item._id !== auth.userId &&
-                    (loginUserFollowing &&
-                    loginUserFollowing.filter((x) => x._id === item._id) ? (
-                      <Button variant='contained' color='primary'>
-                        Following
-                      </Button>
-                    ) : (
-                      <Button variant='contained' color='secondary'>
-                        Follow
-                      </Button>
-                    ))}
-                </div>
-              </div>
-            );
-          })}
-      </Modal> */}
       {
         <div className='user-detail'>
           <Avatar
@@ -199,30 +92,9 @@ const UserPlaces = () => {
               adjective
             </Typography>
             <Follower />
-            {/* <Typography className='follow-section'>
-              <Typography>
-                <Button
-                  onClick={() =>
-                    setShowFollowers((showFollowers) => !showFollowers)
-                  }
-                >
-                  {followers && followers.length} followers
-                </Button>
-              </Typography>
-              <Typography> . </Typography>
-              <Typography>
-                {' '}
-                {following && following.length} following
-              </Typography>
-            </Typography> */}
+       
           </CardContent>
-          {/* <Button
-            variant='contained'
-            color='secondary'
-            onClick={followStatus ? unfollowHandler : followHandler}
-          >
-            {followStatus ? 'You are Following' : 'unFollow'}
-          </Button> */}
+    
         </div>
       }
       {loadedPlaces && userInfo && (
